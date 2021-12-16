9398faef1a23037e9aca04bdaf134f2b1cac2035
Leonard Pop <leonard.pop@stud.ubbcluj.ro>
2021-12-16 16:17:29 +0200

    Add implementation

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from enum import Enum
 
 class codStatus(Enum):
     Q = 0,
-    R = 1,
-    T = 2,
-    E = 3
-
+    R = 1
 
 class elementStivaLucru:
     isTerm = False
@@ -22,21 +19,70 @@ class elementStivaLucru:
 def isNonTerm(x: str, listTerm: set):
     return x in listTerm
 
+def findIndex(nonTerm, listaReguliProductie, minIndex = -1):
+    for index, element in enumerate(listaReguliProductie):
+        if element[0] == nonTerm and index > minIndex:
+            return index
+    
+    return -1
 
 def parcurgere(inputDeVerificat, listaReguliProductie, nonTerminali):
     # Parametrii de lucru
     cod = codStatus.Q
-    index = "0"
+    index = 0
     stivaLucru = []
     stivaIntrare = [listaReguliProductie[0][0]]
     #
 
     while (True):
-        match cod:
-            case codStatus.Q:
-                if isNonTerm(stivaIntrare[-1], nonTerminali):
-                    stivaLucru.append(elementStivaLucru(False))
+        #print(cod)
+        #print([[el.char, el.isTerm, el.index] for el in stivaLucru])
+        #print(stivaIntrare)
 
+        if cod == codStatus.Q:
+            if len(stivaIntrare):
+                element = stivaIntrare[-1]
+
+                if isNonTerm(element, nonTerminali): # expandare
+                    indexRegula = findIndex(element, listaReguliProductie)
+
+                    stivaLucru.append(elementStivaLucru(False,
+                        indexRegula, element))
+                    stivaIntrare.pop()
+                    stivaIntrare.extend(listaReguliProductie[indexRegula][1][::-1])
+                elif index < len(inputDeVerificat) and inputDeVerificat[index] == element: # avans
+                    index += 1
+                    stivaIntrare.pop()
+                    stivaLucru.append(elementStivaLucru(True, None,
+                        element))
+                else: # insucces de moment
+                    cod = codStatus.R
+            else: # success
+                return index
+        elif cod == codStatus.R:
+            elementLucru = stivaLucru.pop()
+
+            if elementLucru.isTerm: # revenire
+                index -= 1
+                stivaIntrare.append(elementLucru.char)
+            else: # alta incercare
+                indexRegula = findIndex(elementLucru.char,
+                        listaReguliProductie, elementLucru.index)
+
+                if indexRegula != -1: # urmatoarea regula
+                    cod = codStatus.Q
+                    stivaLucru.append(elementStivaLucru(False,
+                        indexRegula, elementLucru.char))
+
+                    lungime = len(listaReguliProductie[elementLucru.index][1])
+                    stivaIntrare = stivaIntrare[0:-lungime]
+                    stivaIntrare.extend(listaReguliProductie[indexRegula][1][::-1])
+                elif index == 0 and elementLucru.char == listaReguliProductie[0][0]: # eroare
+                    return -1
+                else: # comprimare
+                    lungime = len(listaReguliProductie[elementLucru.index][1])
+                    stivaIntrare = stivaIntrare[0:-lungime]
+                    stivaIntrare.append(elementLucru.char)
 
 def citireListaProductie():
     nonTerminali = set()
@@ -49,7 +95,9 @@ def citireListaProductie():
             line = line.replace(' ', '')
             parts = line.split("->")
             nonTerminali.add(parts[0])
-            rules.append(parts)
+
+            charList = [char for char in parts[1]]
+            rules.append([parts[0], charList])
     return rules, nonTerminali
 
 
@@ -58,4 +106,10 @@ if __name__ == '__main__':
     print(listaReguliProductie)
     inputDeVerificat = input("Verificati: ")
 
-    parcurgere(inputDeVerificat, listaReguliProductie, nonTerminali)
+    index = parcurgere(inputDeVerificat, listaReguliProductie,
+            nonTerminali)
+
+    if index == len(inputDeVerificat):
+        print("Secventa acceptata")
+    else:
+        print("Secventa respinsa")
